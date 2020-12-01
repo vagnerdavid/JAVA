a79861f40ecd0b8153e48ee9e205f9864297453a
Vágner Dávid <71004491+vagnerdavid@users.noreply.github.com>
2020-12-01 12:38:32 +0100

    Update Sorbarendezes.java

--- sorbarendezes/Sorbarendezes.java
+++ sorbarendezes/Sorbarendezes.java
@@ -27,7 +27,7 @@ public class Sorbarendezes {
 
         //tömb feltöltése - elemek random értéket kapnak
         for (int i = 0; i < elemSzam; i++) {
-            szamok[i] = (int) (Math.random() * 100) - 50;
+            szamok[i] = (int) (Math.random() * 100) - 100;
         }
 
         //tömb kiíratása
